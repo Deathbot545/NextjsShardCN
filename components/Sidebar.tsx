3a95f27820cbb3ea99549d201ec4ccffd49f6a41
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-05-03 18:15:59 +0530

    Route

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,47 +19,6 @@ type props={}
 export default function Sidebar({}:props){
     return(
 <div>
-<Nav
-            isCollapsed={false}
-            links={[
-              {
-                title: "Inbox",
-                label: "128",
-                icon: Inbox,
-                variant: "default",
-              },
-              {
-                title: "Drafts",
-                label: "9",
-                icon: File,
-                variant: "ghost",
-              },
-              {
-                title: "Sent",
-                label: "",
-                icon: Send,
-                variant: "ghost",
-              },
-              {
-                title: "Junk",
-                label: "23",
-                icon: ArchiveX,
-                variant: "ghost",
-              },
-              {
-                title: "Trash",
-                label: "",
-                icon: Trash2,
-                variant: "ghost",
-              },
-              {
-                title: "Archive",
-                label: "",
-                icon: Archive,
-                variant: "ghost",
-              },
-            ]}
-          />
           
 </div>
     )
