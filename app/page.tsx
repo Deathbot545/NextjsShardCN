743cb9368d692743d30fa7a42c2b46b0fd03154b
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-05-03 13:28:28 +0530

    Route

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-
-
+'use client'
+import { useRouter  } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter()
 
-  return (
-    <>
-     
-    </>
-  )
+  useEffect(() => {
+    router.push('/Dashboard');
+  }, []);
+
+  return null;
 }
