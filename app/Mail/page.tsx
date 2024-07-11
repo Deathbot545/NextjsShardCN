ad1b5e897088f9bff7e7b96a70894523a799a9b0
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-07-11 15:25:15 +0530

    less code

--- app/Mail/page.tsx
+++ app/Mail/page.tsx
@@ -2,23 +2,6 @@ import Image from "next/image";
 import { cookies } from "next/headers"
 import { Mail } from "@/components//mail/mail";
 import { accounts, mails } from "@/components/mail/mail-data";
-import TeamSwitcher from "@/components/dashboard/team-switcher";
-import { MainNav } from "@/components/dashboard/main-nav";
-import { Search } from "@/components/dashboard/search";
-import { UserNav } from "@/components/dashboard/user-nav";
-import { CalendarDateRangePicker } from "@/components/dashboard/date-range-picker";
-import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Overview } from "@/components/dashboard/overview";
-import { RecentSales } from "@/components/dashboard/recent-sales";
 export default function Mails(){
     const layout = cookies().get("react-resizable-panels:layout")
   const collapsed = cookies().get("react-resizable-panels:collapsed")
